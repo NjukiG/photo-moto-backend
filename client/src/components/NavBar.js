5ce6c455c9635b4d6c0dd72ace4b3dc80d7345bb
George <gnjuki19@gmail.com>
2023-02-09 13:06:07 +0300

    Added react router dom to build a navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function NavBar() {
   return (
     <div>
         <header>
             <div>
-        
+                <Link to="/">HomePage</Link>
             </div>
+            <
         </header>
     </div>
   )
