3852e412b04678631e79a2de0375241235d12781
George <gnjuki19@gmail.com>
2023-02-10 15:15:36 +0300

    Added routes for the controllers in photos model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,30 @@
 class PhotosController < ApplicationController
+
+    def index
+        photos = Photo.all
+        render json: photos, status: :ok
+    end
+
+    def show
+        photo = @current_user.photos.create!(photo_params)
+        render json: photo, status: :created
+    end
+
+    def update
+        photo = Photo.find_by(id: params[:id])
+        render json: photo, status: :accepted
+    end
+
+    def destroy
+        photo = Photo.find_by(id: params[:id])
+        photo.destroy
+        head :no_content
+    end
+
+    private
+
+    def photo_params
+        params.permit(:title, :image_url)
+    end
+
 end
