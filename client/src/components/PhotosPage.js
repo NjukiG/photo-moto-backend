e6b235ae51b7198f378f9390b6f07e0214deb633
George <gnjuki19@gmail.com>
2023-02-12 00:08:52 +0300

    Implemented a search bar to search for photos on different keywords

--- client/src/components/PhotosPage.js
+++ client/src/components/PhotosPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PhotosForm from "./PhotosForm";
 import PhotosList from "./PhotosList";
+import PhotoSearch from "./PhotoSearch";
 
-function PhotosPage({user}) {
+function PhotosPage({ user }) {
   const [photos, setPhotos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("/photos")
@@ -12,19 +14,34 @@ function PhotosPage({user}) {
   }, []);
 
   const handleAddPhoto = (newPhoto) => {
-      const updatedPhotosArray = [newPhoto, ...photos]
-      setPhotos(updatedPhotosArray)
+    const updatedPhotosArray = [newPhoto, ...photos];
+    setPhotos(updatedPhotosArray);
   };
 
-  const handleDeletePhoto = (photoToDelete) => {
+  const handleDeletePhoto = (id) => {
     setPhotos((photos) => {
-      return photos.filter((photo) => photo.id !== photoToDelete)
-    })
-  }
+      return photos.filter((photo) => photo.id !== id);
+    });
+  };
+
+  const displayedPhotos = photos.filter((photo) => {
+    return photo.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
   return (
     <div>
       <PhotosForm user={user} onAddPhoto={handleAddPhoto} />
-      <PhotosList user={user} photos={photos} onDeletePhoto={handleDeletePhoto} />
+      <PhotoSearch
+        displayedPhotos={displayedPhotos}
+        searchTerm={searchTerm}
+        onSearchChange={setSearchTerm}
+      />
+      <br />
+      <PhotosList
+        user={user}
+        // photos={photos}
+        photos={displayedPhotos}
+        onDeletePhoto={handleDeletePhoto}
+      />
     </div>
   );
 }
